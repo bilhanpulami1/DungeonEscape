2b7a27f6ebda67c80aa38ce777f23501e0010971
bilhanpulami1 <bilhanpulami@gmail.com>
2023-09-25 11:33:15 +0100

    Sounds

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 
 #include "Status.h"
+#include "Point.h"
 
 using namespace std;
 
@@ -116,17 +117,25 @@ void GameMain()
 
 void PlayDoorClosedSound()
 {
-
+	Beep(500, 75);
+	Beep(500, 75);
 }
 void PlayDoorOpenSound()
 {
-
+	Beep(1397, 200);
 }
 void PlayKeyPickupSound()
 {
-
+	Beep(1568, 200);
 }
 void PlayWinSound()
 {
-
+	Beep(1568, 200);
+	Beep(1568, 200);
+	Beep(1568, 200);
+	Beep(1245, 1000);
+	Beep(1397, 200);
+	Beep(1397, 200);
+	Beep(1397, 200);
+	Beep(1175, 1000);
 }
